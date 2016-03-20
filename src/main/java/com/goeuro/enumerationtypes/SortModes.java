e4af1db6330737a782525ac151a2cfdb41d6b277
Andrey Klimachev <andrey.klimachev@feedzai.com>
2016-03-20 17:42:34 +0000

    Create enumeration class sort modes.

---
+++ src/main/java/com/goeuro/enumerationtypes/SortModes.java
@@ -0,0 +1,11 @@
+package com.goeuro.enumerationtypes;
+
+/**
+ * Enumeration that has all possible sorts.
+ *
+ * @author Andrey Klimachev(andre.klim@gmail.com)
+ * @since 0.0.1
+ */
+public enum SortModes {
+    SMARTEST, CHEAPEST, FASTEST, DEPARTURE_TIME, ARRIVAL_TIME
+}
